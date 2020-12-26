2dbeed4550380c8354538ecfdd1fbe24abff2c05
DimitrisSyrros <dimsyrr@gmail.com>
2020-12-26 10:35:29 +0200

    Addition of Highlighted Skills

--- pages/about-me.js
+++ pages/about-me.js
@@ -4,6 +4,8 @@ import '../styles/AboutMePage.module.scss'
 import { timelineData } from './timeline'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faBriefcase, faUniversity } from '@fortawesome/free-solid-svg-icons'
+import { faPhp, faReact, faPython, faSass, faHtml5 } from '@fortawesome/free-brands-svg-icons'
+import Image from 'next/image'
 
 class AboutMePage extends React.Component {
     render() {
@@ -16,20 +18,52 @@ class AboutMePage extends React.Component {
                 }
                 <span className="timestamp">{item.timeStamp}</span>
                 <h4 className="entry-title">{item.entryTitle}</h4>
+                <p className="sub-header">{item.subHeader}</p>
                 <p className="details">{item.details}</p>
+                {
+                    item.details && <button className="readMoreBtn">Read More</button>
+                }
             </article>
         );
         return (
             <div className='container'>
                 <Navbar />
-                <main>
+                <div>
                     <h2 className="header">About <span className="yellow">Me</span></h2>
                     <h1 className="shadow">Resume</h1>
                     <h3>Education <span className="yellow">&</span> Experience</h3>
                     <div className="timeline-grid">
                         {list}
                     </div>
-                </main>
+                    <h3><span className="highlighted">Highlighted </span>Skills</h3>
+                    <div className="languages">
+                        <div className="box">
+                            <h5 className="name">React</h5>
+                            <FontAwesomeIcon icon={faReact} size={'3x'} color={"#61DBFB"} />
+                        </div>
+                        <div className="box">
+                            <h5 className="name">PHP</h5>
+                            <FontAwesomeIcon icon={faPhp} size={'3x'} color={"#787CB5"} />
+                        </div>
+                        <div className="box">
+                            <h5 className="name">HTML</h5>
+                            <FontAwesomeIcon icon={faHtml5} size={'3x'} color={"#E44D26"} />
+                        </div>
+                        <div className="box">
+                            <h5 className="name">SCSS</h5>
+                            <FontAwesomeIcon icon={faSass} size={'3x'} color={"#CD6799"} />
+                        </div>
+                        <div className="box">
+                            <h5 className="name">Python</h5>
+                            <FontAwesomeIcon icon={faPython} size={'3x'} color={"#FFE873"} />
+                        </div>
+                        <div className="box">
+                            <h5 className="name">Go</h5>
+                            <Image src="/Go-Logo_Aqua.png" alt="golang" layout='fill' />
+                        </div>
+                    </div>
+
+                </div>
             </div>
         );
     }
--- pages/timeline.js
+++ pages/timeline.js
@@ -2,34 +2,37 @@ export const timelineData = [
     {
         "timeStamp": "2009",
         "entryTitle": "Certificate of Proficiency in English from the University of Michigan",
-        "details": "Level: C2 Overall Grade of Degree PASS",
+        "subHeader": "Level: C2 Overall Grade of Degree PASS",
         "type": "faUniversity"
     },
     {
         "timeStamp": "From 2009 to 2012",
         "entryTitle": "General Lyceum - General High School Diploma",
-        "details": "Grade Excellent (19)",
+        "subHeader": "Grade Excellent (19)",
         "type": "faUniversity"
     },
     {
         "timeStamp": "From September/2012 To April/2019",
-        "entryTitle": "Studied at the Computer Engineering & Informatics Department of the University of Patras",
-        "details": "Integrated Masters Degree",
+        "entryTitle": "Integrated Masters Degree - Computer Engineering & Informatics Department (CEID) of the University of Patras",
+        "details": "As part of my thesis dissertation titled : Detecting cyber-bullying on social networks using natural language processing and machine learning...",
         "type": "faUniversity"
     },
     {
         "timeStamp": "From April/2019 to January/2020",
-        "entryTitle": "Worked for Intrasoft Intl. as a Junior Software Engineer",
+        "entryTitle": "Intrasoft Intl. - Software Engineer",
+        "details": "Worked as a web developer on high-scale, multi-tenant projects in the field of betting...",
         "type": "faBriefcase"
     },
     {
         "timeStamp": "From January/2020 to October/2020",
-        "entryTitle": "Served in Greek Millitary as Software Engineer/Ifantry Soldier",
+        "entryTitle": "Centre of Iformation Techonology Support of the Hellenic Army (KEPYES) - Software Engineer/Ifantry Soldier",
+        "details": "As a Greek citizen i  served for a 9-month term for the Greek Army where...",
         "type": "faBriefcase"
     },
     {
         "timeStamp": "From November 2020 untill now",
-        "entryTitle": "Working for Intrasoft Intl. as a Junior Software Engineer",
+        "entryTitle": "Intrasoft Intl. - Software Engineer",
+        "details": "Currently employed as a web developer on high-scale, multi-tenant projects in the field of betting...",
         "type": "faBriefcase"
     }
 ]
