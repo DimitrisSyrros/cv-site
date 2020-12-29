99daa96303277224cd0018d402c6baab2f1ec98b
DimitrisSyrros <dimsyrr@gmail.com>
2020-12-29 13:04:15 +0200

    Finished highlighted skills section

--- pages/about-me.js
+++ pages/about-me.js
@@ -6,6 +6,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faBriefcase, faUniversity } from '@fortawesome/free-solid-svg-icons'
 import { faPhp, faReact, faPython, faSass, faHtml5 } from '@fortawesome/free-brands-svg-icons'
 import Image from 'next/image'
+import Title from 'react-vanilla-tilt'
 
 class AboutMePage extends React.Component {
     render() {
@@ -13,8 +14,8 @@ class AboutMePage extends React.Component {
             <article key={index} className="timeline-entry">
                 {
                     item.type === "faUniversity" ?
-                        <FontAwesomeIcon icon={faUniversity} color={"#ffb400"} />
-                        : <FontAwesomeIcon icon={faBriefcase} color={"#ffb400"} />
+                        <FontAwesomeIcon className="lang-logo" icon={faUniversity} color={"#ffb200"} />
+                        : <FontAwesomeIcon className="lang-logo" icon={faBriefcase} color={"#ffb200"} />
                 }
                 <span className="timestamp">{item.timeStamp}</span>
                 <h4 className="entry-title">{item.entryTitle}</h4>
@@ -37,30 +38,46 @@ class AboutMePage extends React.Component {
                     </div>
                     <h3><span className="highlighted">Highlighted </span>Skills</h3>
                     <div className="languages">
-                        <div className="box">
-                            <h5 className="name">React</h5>
-                            <FontAwesomeIcon icon={faReact} size={'3x'} color={"#61DBFB"} />
-                        </div>
-                        <div className="box">
-                            <h5 className="name">PHP</h5>
-                            <FontAwesomeIcon icon={faPhp} size={'3x'} color={"#787CB5"} />
-                        </div>
-                        <div className="box">
-                            <h5 className="name">HTML</h5>
-                            <FontAwesomeIcon icon={faHtml5} size={'3x'} color={"#E44D26"} />
-                        </div>
-                        <div className="box">
-                            <h5 className="name">SCSS</h5>
-                            <FontAwesomeIcon icon={faSass} size={'3x'} color={"#CD6799"} />
-                        </div>
-                        <div className="box">
-                            <h5 className="name">Python</h5>
-                            <FontAwesomeIcon icon={faPython} size={'3x'} color={"#FFE873"} />
-                        </div>
-                        <div className="box">
-                            <h5 className="name">Go</h5>
-                            <Image src="/Go-Logo_Aqua.png" alt="golang" layout='fill' />
-                        </div>
+                        <Title className="skill" className="skill" options={{ max: 50, speed: 200 }}>
+                            <div className="box">
+                                <h5 className="name">React</h5>
+                                <FontAwesomeIcon className="lang-logo" icon={faReact} size={'3x'} color={"#61DBFB"} />
+                            </div>
+                        </Title>
+                        <Title className="skill" className="skill" options={{ max: 50, speed: 200 }}>
+                            <div className="box">
+                                <h5 className="name">PHP</h5>
+                                <FontAwesomeIcon className="lang-logo" icon={faPhp} size={'3x'} color={"#787CB5"} />
+                            </div>
+                        </Title>
+                        <Title className="skill" className="skill" options={{ max: 50, speed: 200 }}>
+
+                            <div className="box">
+                                <h5 className="name">HTML</h5>
+                                <FontAwesomeIcon className="lang-logo" icon={faHtml5} size={'3x'} color={"#E44D26"} />
+                            </div>
+                        </Title>
+                        <Title className="skill" className="skill" options={{ max: 50, speed: 200 }}>
+
+                            <div className="box">
+                                <h5 className="name">SCSS</h5>
+                                <FontAwesomeIcon className="lang-logo" icon={faSass} size={'3x'} color={"#CD6799"} />
+                            </div>
+                        </Title>
+                        <Title className="skill" className="skill" options={{ max: 50, speed: 200 }}>
+
+                            <div className="box">
+                                <h5 className="name">Python</h5>
+                                <FontAwesomeIcon className="lang-logo" icon={faPython} size={'3x'} color={"#FFE873"} />
+                            </div>
+                        </Title>
+                        <Title className="skill" className="skill" options={{ max: 50, speed: 200 }}>
+
+                            <div className="box">
+                                <h5 className="name">Go</h5>
+                                <Image src="/Go-Logo_Aqua.png" alt="golang" width="100" height="50" />
+                            </div>
+                        </Title>
                     </div>
 
                 </div>
