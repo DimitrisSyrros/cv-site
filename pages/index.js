bbfa3c3a1911314e6c92d59d6c4c397305026de8
DimitrisSyrros <dimsyrr@gmail.com>
2021-07-06 21:58:22 +0300

    Removed eventListener resize

--- pages/index.js
+++ pages/index.js
@@ -13,12 +13,6 @@ const Home = () => {
     const aboutMeRef = useRef(null)
     const contactRef = useRef(null)
     useEffect(() => {
-        window.addEventListener("resize", function () {
-            let vh = window.innerHeight * 0.01;
-            document.documentElement.style.setProperty('--vh', `${vh}px`);
-            let vw = window.innerWidth * 0.01;
-            document.documentElement.style.setProperty('--vw', `${vw}px`);
-        });
         let vh = window.innerHeight * 0.01;
         document.documentElement.style.setProperty('--vh', `${vh}px`);
         let vw = window.innerWidth * 0.01;
